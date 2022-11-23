462f852e8702628b067c5ebadc6551bba53aee86
SystemProjetos4002 <118873311+SystemProjetos4002@users.noreply.github.com>
2022-11-23 19:00:37 -0300

    Testes para conexão e retorno dos dados do Banco

--- conection.py
+++ conection.py
@@ -5,6 +5,7 @@ def conecta(host,user,password,database):
     try:
         db_connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
         print("Database connection made!")
+        return db_connection
     except mysql.connector.Error as error:
         if error.errno == errorcode.ER_BAD_DB_ERROR:
             print("Database doesn't exist")
@@ -14,3 +15,4 @@ def conecta(host,user,password,database):
             print(error)
     else:
         db_connection.close()
+    
--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import conection
-print('hello')
+import conection as c
+
+conexao = c.conecta('152.70.214.62','Hildean.Dantas','cachoromolhado','PROJETO_RH')
+
+cursor = conexao.cursor(buffered = True) 
+
+sql = "SELECT * FROM t_area;"
+
+cursor.execute(sql)
+
+final_result = [i for i in cursor.fetchall()]
+
+print(final_result)
+  
+cursor.close()
+conexao.commit()
+conexao.close()
