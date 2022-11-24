0f8479972331fd9d7cb4e619b0c38dc6044338aa
SystemProjetos4002 <118873311+SystemProjetos4002@users.noreply.github.com>
2022-11-24 14:09:27 -0300

    implantação dos js na pagina

--- web/js/index.js
+++ web/js/index.js
@@ -1,8 +1,48 @@
-// Onclick of the button
-document.querySelector("button").onclick = function () {  
-    // Call python's random_python function
-    eel.random_python()(function(number){                      
-      // Update the div with a random number returned by python
-      document.querySelector(".random_number").innerHTML = number;
-    })
-  }
+var login = document.querySelector("login")
+var senha = document.querySelector("senha")
+var button = document.getElementById("confirmar")
+
+var login_cadastro = 'aa'
+var senha_cadastro = '123'
+
+// function verifica_login(login) {
+//     if(login == login_cadastro) {
+//         return true
+//     }
+// }
+
+// function verifica_senha(senha) {
+//     if(senha == senha_cadastro) {
+//         return true
+//     }
+// }
+
+document.getElementById('login').onkeypress = function (e) {
+    if (e.keyCode == 13) {
+        libera_acesso();
+        e.preventDefault();
+    }
+   
+}
+
+document.getElementById('senha').onkeypress = function (e) {
+    if (e.keyCode == 13) {
+        libera_acesso();
+        e.preventDefault();
+    }
+   
+}
+
+document.getElementById("confirmar").onclick = function(e) {
+    libera_acesso();
+    e.preventDefault();
+}
+
+function libera_acesso (login,senha) {
+    if(login === login_cadastro && senha === senha_cadastro) {
+        window.alert('Acesso liberado!');
+    } 
+    else {
+        window.alert('Credenciais incorretas, verifique e tente novamente!')
+    }
+}
